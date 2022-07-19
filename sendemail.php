e9ff08f486b54b2485da87361c22d21164a4577b
VTMM <109035723+vitaltechmm@users.noreply.github.com>
2022-07-19 16:58:22 +0630

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -10,7 +10,7 @@ $mail = new PHPMailer(true);
 $alert = '';
 
 if(isset($_POST['submit'])){
-echo  $name = $_POST['name'];
+  $name = $_POST['name'];
   $surname = $_POST['surname'];
   $email = $_POST['email'];
   $subject = $_POST['subject'];
